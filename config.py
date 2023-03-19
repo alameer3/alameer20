3418b8e993e8ba141b3b8e6928a494ea81ddbcc3
alameer3 <113803842+alameer3@users.noreply.github.com>
2023-03-19 18:54:17 +0300

    Create config.py

---
+++ config.py
@@ -0,0 +1,10 @@
+import  os
+
+
+BOT_TOKEN = os.environ.get("6011254166:AAHPo2TGtPMSUKAepH4yoXDY-xWfEdzdRX8")
+APP_ID = int(os.environ.get("15172934"))
+API_HASH = os.environ.get("api_hash:9a46cc4d2e1357e593b11266de9701f3")
+
+youtube_next_fetch = 0  # time in minute
+
+EDIT_TIME = 5
